84340a6188c1164644057253940a5671d993d9c5
Fayez Nassar <fayeznas@gmail.com>
2016-11-30 16:09:38 +0200

    delete app.json

--- server.py
+++ server.py
@@ -33,7 +33,3 @@ def first_connection():
         print 'Got connection from', addr
         c.send('Thank you for connecting')
         c.close()
-
-
-if __name__ == '__main__':
-    first_connection()
