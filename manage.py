c74fed94469bd689fe2ba7882bd373d28adee916
Fayez Nassar <fayeznas@gmail.com>
2016-11-30 16:54:51 +0200

    update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import server
 if __name__ == "__main__":
-    server.first_connection()
+    print "Hello World"
+    # server.first_connection()
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server")
     # from django.core.management import execute_from_command_line
     # execute_from_command_line(sys.argv)
