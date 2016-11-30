c227ee611e28b9906d9719a508803c07b8899aee
Fayez Nassar <fayeznas@gmail.com>
2016-11-30 17:38:16 +0200

    update Procfile

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import server
+import os
+import sys
+
 if __name__ == "__main__":
-    print "Hello World"
-    # server.first_connection()
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server")
-    # from django.core.management import execute_from_command_line
-    # execute_from_command_line(sys.argv)
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
