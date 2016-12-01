ece593dee92dfa9968abfe8b772a01062da430d7
Fayez Nassar <fayeznas@gmail.com>
2016-12-01 19:52:25 +0200

    asas

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -15,5 +15,6 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
+print "here"
 application = server.first_connection(application)
