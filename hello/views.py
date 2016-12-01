99b2eb2201eb2a247509a40584ebe14d6f52b756
Fayez Nassar <fayeznas@gmail.com>
2016-12-01 22:03:03 +0200

    asasfSA

--- hello/views.py
+++ hello/views.py
@@ -3,18 +3,12 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
-# Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
-
+    return HttpResponse('Hello Deep learning server!')
 
 def db(request):
-
     greeting = Greeting()
     greeting.save()
-
     greetings = Greeting.objects.all()
-
     return render(request, 'db.html', {'greetings': greetings})
 
